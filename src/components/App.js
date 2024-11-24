7d5f8b43c21b9fea628717b39e43493eac2cfc9c
patilvishal2507 <117289866+patilvishal2507@users.noreply.github.com>
2024-11-24 23:06:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,6 @@ const App = ()=>{
    return (
      <div id="main">
      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-     </div
+     </div>
      }
 export default App
